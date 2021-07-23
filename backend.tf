2d6089ff4af7c2fa6eed4c09a337a5f57fcf1833
bKubri27 <85324691+bKubri27@users.noreply.github.com>
2021-07-22 20:58:12 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "	terraform-jenkins100"
+    bucket = "terraform-jenkins100"
     key = "jt.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraforn-jenk"
